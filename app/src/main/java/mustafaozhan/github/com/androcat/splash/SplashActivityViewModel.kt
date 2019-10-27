e30a0119100ba3a8a0051260072f9881c1ec4324
Rebecca A. Esquivel <42479478+BAEsquivel@users.noreply.github.com>
2019-10-27 06:28:18 -0700

    Delete SplashActivityViewModel.kt

--- app/src/main/java/mustafaozhan/github/com/androcat/splash/SplashActivityViewModel.kt
+++
@@ -1,7 +0,0 @@
-package mustafaozhan.github.com.androcat.splash
-
-import mustafaozhan.github.com.androcat.base.BaseViewModel
-
-class SplashActivityViewModel : BaseViewModel() {
-    override fun inject() = viewModelComponent.inject(this)
-}
