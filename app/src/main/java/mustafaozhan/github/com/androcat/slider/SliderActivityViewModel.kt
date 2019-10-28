6e74538d9c4566d68926c6432acdfc9c7d319b86
Rebecca A. Esquivel <42479478+BAEsquivel@users.noreply.github.com>
2019-10-28 01:41:58 -0700

    Delete SliderActivityViewModel.kt

--- app/src/main/java/mustafaozhan/github/com/androcat/slider/SliderActivityViewModel.kt
+++
@@ -1,9 +0,0 @@
-package mustafaozhan.github.com.androcat.slider
-
-import mustafaozhan.github.com.androcat.base.BaseViewModel
-
-class SliderActivityViewModel : BaseViewModel() {
-    override fun inject() {
-        viewModelComponent.inject(this)
-    }
-}
